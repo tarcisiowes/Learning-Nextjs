b736b381848d564730aedff1f6842e1797505a55
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-10-12 12:43:56 -0300

    arquivo exemplo para aplicar css

---
+++ 1st-next/pages/css/integracao.jsx
@@ -0,0 +1,9 @@
+export default function integracao() {
+  return (
+    <div>
+      <div className="vermelha">texto</div>
+      <div className="azul">texto</div>
+      <div className="branca">texto</div>
+    </div>
+  )
+}
