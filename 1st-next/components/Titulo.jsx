ae3e1f4ced1d7cc83bfb12614de6b9e12f89989a
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-10-12 11:45:32 -0300

    componente com ternario

--- 1st-next/components/Titulo.jsx
+++ 1st-next/components/Titulo.jsx
@@ -1,8 +1,13 @@
-export default function Titulo() {
-  return (
+export default function Titulo(props) {
+  return props.pequeno ? (
     <>
-    <h1>Titulo</h1>
-    <h2>Subtitulo</h2>
+      <h3>{ props.principal }</h3>
+      <h4>{ props.detalhe }</h4>
     </>
-  )
+    ) : (
+    <>
+      <h1>{ props.principal }</h1>
+      <h2>{ props.detalhe }</h2>
+    </>
+   )
 }
