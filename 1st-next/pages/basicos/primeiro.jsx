1dbdc6bd1ee38212534e6b3d3e704b72583b4fde
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-10-12 11:47:17 -0300

    utilizando o mesmo componente de diferente formas

--- 1st-next/pages/basicos/primeiro.jsx
+++ 1st-next/pages/basicos/primeiro.jsx
@@ -3,7 +3,17 @@ import Titulo from "../../components/Titulo"
 export default function primeiro() {
   return (
     <>
-      <Titulo/>
+      <Titulo
+        principal="Página de cadastro"
+        detalhe="Incluir, alterar e excluir coisas!"      
+      />
+      
+      <Titulo
+        principal="Página de login"
+        detalhe="validar, entrar e visualizar coisas!"
+        pequeno
+      />
+      
       Primeiro componente
     </>
   )
