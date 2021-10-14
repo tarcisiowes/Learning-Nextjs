672737c295d16f44faf9bcb48989ee51a877bb9d
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-10-14 12:24:03 -0300

    reutilizando mesmo componente

--- 1st-next/pages/css/usandoEstiloCondicional.jsx
+++ 1st-next/pages/css/usandoEstiloCondicional.jsx
@@ -5,6 +5,7 @@ export default function usandoEstiloCondicional() {
   return (
     <div>
       <EstiloCondicional numero={5} color="#000" direita />
+      <EstiloCondicional numero={-1} color="#fff" />
     </div>
   )
 }
