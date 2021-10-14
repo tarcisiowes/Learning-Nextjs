bc9df1e097f7ced7cc5d989eead2588311980298
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-10-14 12:17:44 -0300

    pg + utilizando estilo condicional

---
+++ 1st-next/pages/css/usandoEstiloCondicional.jsx
@@ -0,0 +1,10 @@
+import EstiloCondicional from "../../components/estiloCondicional"
+
+export default function usandoEstiloCondicional() {
+  
+  return (
+    <div>
+      <EstiloCondicional numero={5} />
+    </div>
+  )
+}
